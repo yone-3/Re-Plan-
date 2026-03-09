49087af6b083ee81e0771066737059c655e75cb0
Ehara <mnrehr0913@gmail.com>
2026-03-09 14:04:40 +0900

    0309AM

---
+++ footer.php
@@ -0,0 +1,83 @@
+    <footer class="l-footer__pc">
+        <a href="./index.html">
+            <img class="c-footer-logo" src="./image/logo-footer.png" alt="リプランエイト">
+        </a>
+        <div class="c-footer-sns">
+            <ul>
+                <li class="c-logo__insta">
+                    <a href="">
+                        <img src="./image/logo-insta__footer.svg" alt="インスタグラム">
+                    </a>
+                </li>
+                <li class="c-logo__you">
+                    <a href="">
+                        <img src="./image/logo-you__footer.svg" alt="ユーチューブ">
+                    </a>
+                </li>
+                <li class="c-logo__tik">
+                    <a href="">
+                        <img src="./image/logo-tiktok__footer.svg" alt="ティックトック">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="c-footer-nav">
+            <ul>
+                <li class="c-footer-nav__list">
+                    <a href="./index.html">TOP</a>
+                </li>
+                <li class="c-footer-nav__list">
+                    <a href="./project.html">PROJECT</a>
+                </li>
+                <li class="c-footer-nav__list">
+                    <a href="./news.html">NEWS</a>
+                </li>
+                <li class="c-footer-nav__list">
+                    <a href="./schedule.html">SCHEDULE</a>
+                </li>
+                <li class="c-footer-nav__list">
+                    <a href="./members.html">MEMBERS</a>
+                </li>
+                <li class="c-footer-nav__list">
+                    <a href="./discography.html">DISCOGRAPHY</a>
+                </li>
+                <li class="c-footer-nav__list">
+                    <a href="./contact.html">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+        <small class="c-copy">&copy;Re:Plan∞ / Infinity Culture Lab</small>
+        <p class="c-copy">
+            Observation Log archived by Infinity Culture Lab
+        </p>
+
+    </footer>
+    <footer class="l-footer__sp">
+        <a href="./index.html">
+            <img class="c-footer-logo" src="./image/logo-footer.png" alt="リプランエイト">
+        </a>
+        <div class="c-footer-sns">
+            <ul>
+                <li class="c-logo__insta">
+                    <a href="">
+                        <img src="./image/logo-insta__footer.svg" alt="インスタグラム">
+                    </a>
+                </li>
+                <li class="c-logo__you">
+                    <a href="">
+                        <img src="./image/logo-you__footer.svg" alt="ユーチューブ">
+                    </a>
+                </li>
+                <li class="c-logo__tik">
+                    <a href="">
+                        <img src="./image/logo-tiktok__footer.svg" alt="ティックトック">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <small class="c-copy">&copy;Re:Plan∞ / Infinity Culture Lab</small>
+        <p class="c-copy">
+            Observation Log archived by Infinity Culture Lab
+        </p>
+
+    </footer>
---
+++ header.php
@@ -0,0 +1,47 @@
+    <header class="l-header__pc">
+        <h1 class="c-header-logo">
+            <a href="./index.html">
+                <img src="./image/logo.png" alt="リプランエイト">
+            </a>
+        </h1>
+        <nav>
+            <div class="c-header__pc">
+                <ul class="c-header-nav">
+                    <li class="c-header-nav__list">
+                        <a href="./index.html">TOP</a>
+                    </li>
+                    <li class="c-header-nav__list">
+                        <a href="./project.html">PROJECT</a>
+                    </li>
+                    <li class="c-header-nav__list">
+                        <a href="./news.html">NEWS</a>
+                    </li>
+                    <li class="c-header-nav__list">
+                        <a href="./schedule.html">SCHEDULE</a>
+                    </li>
+                    <li class="c-header-nav__list">
+                        <a href="./members.html">MEMBERS</a>
+                    </li>
+                    <li class="c-header-nav__list">
+                        <a href="./discography.html">DISCOGRAPHY</a>
+                    </li>
+                    <li class="c-header-nav__list">
+                        <a href="./contact.html">CONTACT</a>
+                    </li>
+                    <li class="c-header-nav__list c-logo__insta">
+                        <a href="">
+                            <img src="./image/logo-insta__header.svg" alt="インスタグラム">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <header class="l-header__sp">
+        <h1 class="c-header-logo">
+            <a href="./index.html">
+                <img src="./image/logo.png" alt="リプランエイト">
+            </a>
+        </h1>
+        <img src="./image/hamburger.svg" alt="">
+    </header>
